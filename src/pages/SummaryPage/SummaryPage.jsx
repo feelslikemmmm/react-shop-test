8bb6b7886af5f09ce4c5e5d26abc9175af9ddcfd
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-14 02:18:58 +0900

    feat: SummaryPage test

--- src/pages/SummaryPage/SummaryPage.jsx
+++ src/pages/SummaryPage/SummaryPage.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SummaryPage = () => {
-  return <div></div>;
+  const [checked, setCheked] = useState(false);
+
+  return (
+    <div>
+      <form action="">
+        <input
+          type="checkbox"
+          name="checkbox"
+          id="confirm-checkbox"
+          checked={checked}
+          onChange={(e) => setCheked(e.target.checked)}
+        />
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button disabled={!checked} type="submit">
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SummaryPage;
